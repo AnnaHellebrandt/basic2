9c6033c655c04d960a4cb2db91e071b8568002bf
Anna <anna.hellebrandt@fingo.pl>
2014-11-28 09:26:54 +0100

    Initial commit

---
+++ src/Ania.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Ania on 2014-11-27.
+ */
+public class Ania {
+    public static void main(String[] args) {
+        String[] powitanie = new String[3];
+        powitanie[0] = "Witaj w Core Java";
+        powitanie[1] = "właściciel: Ziutek Krawężnik";
+        powitanie[2] = "i Ziomek Ziomkowski";
+    }
+}
+
